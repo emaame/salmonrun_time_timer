1618b9e28e63acd0580c2433e6a9ccb833af4265
emaame <ameema@gmail.com>
2017-12-17 21:21:06 +0900

    refactor listup_next_STT

--- src/salmonrun_time_timer.js
+++ src/salmonrun_time_timer.js
@@ -1,6 +1,8 @@
 class SalmonrunTimeTimer {
     constructor() {
     }
+
+    // テストしやすいように基準の日時を渡せるようにした
     listup_next_STT(date = Date.now()) {
         var list = [];
         var base = new Date(date);
@@ -8,31 +10,27 @@ class SalmonrunTimeTimer {
         //  period |   0   |            1            | 2  ...  6 |            7            | 8
         // minutes | 00 01 | 02 03 04 05 06 07 08 09 | 10 ... 49 | 50 51 52 53 54 55 56 57 | 58 59
         var period = Math.floor( (base.getMinutes() - 2) / 8) + 1; // it becomes 0 to 8
-        var makeDate = (hours, minutes) => {
-            var d = new Date(base);
-            d.setHours(hours);
-            d.setMinutes(minutes);
-            d.setSeconds(0);
-            d.setMilliseconds(0);
-            return d;
-        };
-
+        
         // in this hour (upto 1st(:02),2nd(:10), ... ,7th(:50))
-        var count = 0;
         var hours = base.getHours();
-        var i, minutes, d;
-        for(i = period+1; i <= 7; ++i, ++count) {
-            minutes = 2 + (i-1) * 8;
-            d = makeDate( hours, minutes );
-            list.push( d );
-        }
-        // in next hour
-        for(i = 1; count<7; ++i, ++count) {
-            minutes = 2 + (i-1) * 8;
-            d = makeDate( hours+1, minutes );
-            list.push( d );
-        }
 
+        // iterate about hours
+        for(; list.length < 7; ) {
+            // in this hour
+            // i が 8 なら次の時間へ、もしくは 7 件埋まれば終了
+            for(var i = period+1; i <= 7 && list.length < 7; ++i) {
+                var minutes = 2 + (i-1) * 8;
+                var d = new Date(base);
+                d.setHours(hours);
+                d.setMinutes(minutes);
+                d.setSeconds(0);
+                d.setMilliseconds(0);
+                list.push( d );
+            }
+            // for next hour
+            period = 0;
+            hours += 1;
+        }
         return list;
     }
 }
