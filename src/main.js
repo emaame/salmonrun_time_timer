17e89df9b873582b1ce3ebff0d089bb4df4b50b6
emaame <ameema@gmail.com>
2017-12-24 17:48:51 +0900

    append JST clock

--- src/main.js
+++ src/main.js
@@ -1,22 +1,41 @@
 import SalmonrunTimeTimer from "./salmonrun_time_timer";
 import DateFormatter from "./date_formatter";
+import TimeOffset from "./time_offset";
 
 const date_formatter = new DateFormatter();
 
 class App {
     constructor() {
-        this.timer = new SalmonrunTimeTimer();
+        this.time_offset = new TimeOffset();
+        this.timer = new SalmonrunTimeTimer(this.time_offset);
+
+        document.getElementById("toggle_time_offset_information").addEventListener( "click", this.toggle_jst_detail.bind(this) );
     }
     calc_eta() {
         const base = new Date(Date.now());
         this.list = this.timer.listup_next_STT();
-        this.eta = new Date(this.list[0] - base);
+        var eta = this.list[0] - base;
+        if (this.time_offset.offset) { eta += this.time_offset.offset; }
+        this.eta = new Date(eta);
     }
     update_eta() {
         // eta
         const elmEta = document.getElementById("eta");
         const textEta = date_formatter.getMinText(this.eta);
         elmEta.innerHTML = textEta;
+        // offset
+        const elmOffset = document.getElementById("offset");
+        if (this.time_offset.offset) {
+            var textOffset;
+            if (this.time_offset.offset < 0) {
+                textOffset = "- " + date_formatter.getMinText(new Date(-this.time_offset.offset));
+            } else {
+                textOffset = "+ " + date_formatter.getMinText(new Date( this.time_offset.offset));
+            }
+            elmOffset.innerHTML = textOffset + " を補正済み";
+        } else {
+            elmOffset.innerHTML = "時刻合わせ中 ...";
+        }
     }
     update_list() {
         // list
@@ -36,6 +55,12 @@ class App {
             interval = 50;
         }
         setTimeout(this.update.bind(this), interval);
+    }
+    toggle_jst_detail() {
+        var msgbox = document.getElementById("jst_detail");
+        if ( msgbox == null ) return;
+        if( msgbox.style.display == "block" ) msgbox.style.display = "none"
+        else msgbox.style.display = "block";
     }
 }
 
---
+++ src/time_offset.js
@@ -0,0 +1,104 @@
+// based on https://www.nict.go.jp/JST/JST5.js
+
+var ServerList = [
+    "https://ntp-a1.nict.go.jp/cgi-bin/json",
+    "https://ntp-b1.nict.go.jp/cgi-bin/json",
+    "https://ntp-a4.nict.go.jp/cgi-bin/json"
+];
+import getJSON from "get-json-data";
+
+class TimeOffset {
+    constructor() {
+        this.error = "";
+        this.status = 0;	// 1: 1 server only, 2: accurate
+        this.offset = 0;
+        this.get_offset();
+    }
+    
+    get_offset() {
+        this.results = new Array();
+        this.responsed_count = 0;
+        for (let server of ServerList) {
+            getJSON(server + "?" + ( (new Date()).getTime() / 1000 ), (err, json) => {
+                ++this.responsed_count;
+                if (err) {
+                    this.error += String(err) + "\n";
+                    this.errmsg(this.error);
+                    return;
+                }
+                var now = new Date();	 		// Receive time
+                if( json.st && json.it && json.leap && json.next && json.step ) {
+                    json.rt = now.getTime(); 		// Receive time
+                    json.it = Number(json.it) * 1000; 			// Initiate time
+                    json.st = Number(json.st) * 1000; 			// Send time
+                    json.leap = Number(json.leap);
+                    json.next = Number(json.next);
+                    json.step = Number(json.step);
+                    json.rtt = json.rt - json.it;		// Round Trip Time
+                    json.dif = json.st - (json.it+json.rt)/2;	// estimated clock difference
+                    json.lb = json.it - 16 - json.st;	// estimated lower bound
+                    json.ub = json.rt + 16 - json.st;	// estimated upper bound
+                    this.results.unshift( json );		// json -> Result[0]
+                }
+                if (this.responsed_count >= 2) {
+                    this.calculate();
+                }
+            });
+        }
+    }
+    errmsg(msgnum) {
+        var elm = document.getElementById("jst_error");
+        if (elm) { elm.innerHTML = msgnum; }
+    }
+    calculate() {
+        var maxlb, minub;
+        var results = this.results;
+    
+        if ( results.length == 0 ) return -1;
+    
+        if ( results.length >= 2 ) {	// Error should be less than 500ms
+            for (var i = 1; i < results.length; i++) {
+                maxlb = Math.max( results[0].lb, results[i].lb );
+                minub = Math.min( results[0].ub, results[i].ub );
+                if( (maxlb < minub) && ((minub - maxlb) < 500)) {
+                    this.status = 2;
+                    this.errmsg("normal");
+                    break;
+                }
+            }
+        }
+        if (this.status != 2 ){	// Otherwise set status = 1 and try next server
+            maxlb = results[0].lb;
+            minub = results[0].ub;
+            this.status = 1;
+            this.errmsg("warning");
+        }
+    
+        this.offset = - ( maxlb + minub ) / 2;
+        this.leap = results[0].leap;  // Total leap seconds before NEXT Leap
+        this.next = results[0].next;  // Next Leap (UNIX TIME)
+    
+        var start = Math.floor( ( this.offset + (new Date()).getTime() ) / 1000 );
+    
+        if ( start >= this.next ) { this.sl = this.leap + 1; }	// TAI - UTC at startup
+        else                      { this.sl = this.leap; }
+    
+        // detail status
+        var msg  = "";
+        for (let i = results.length - 1; i >= 0; i--) {
+            msg += results[i].id + ": RTT = " + Math.round( results[i].rtt ) + " ms , (PC Clock - JST) = " +
+                    Math.round( ( results[i].lb + results[i].ub ) / 2 ) + " ms<br>";
+        }
+    
+        msg += "Estimated clock difference (PC Clock - JST) = " + Math.round( - this.offset ) + " &plusmn; " + Math.ceil( ( minub - maxlb ) / 2 ) + "ms<br>"
+    
+        var jst_status = document.getElementById( "jst_status" );
+        if ( jst_status ) { jst_status.innerHTML = msg; }
+    }
+    
+
+
+}
+
+
+export default TimeOffset;
