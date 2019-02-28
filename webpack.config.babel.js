f678b2ed9ce3cacb3e16462ab45924d3f4df650d
ema <ameema@gmail.com>
2019-03-01 02:25:56 +0900

    update webpack and babel versionn

---
+++ webpack.config.babel.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+import path from 'path'
+
+export default {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js',
+  output: {
+    path: path.join(__dirname, '/docs'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ],
+  },
+  resolve: {
+    modules: [ 'node_modules', path.resolve(__dirname, 'src/') ],
+    extensions: ['.js'],
+  },
+  devServer: {
+    inline: true,
+    contentBase: path.resolve(__dirname, 'docs'),
+    watchContentBase: true,
+    hot: true,
+    open: true,
+    port: 8888,
+  },
+  devtool: 'inline-source-map',
+  optimization: {
+    minimize: true,
+  },
+}
