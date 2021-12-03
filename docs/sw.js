90b17d1d801f5461832d8e1af331dc48343a0caa
emaame <emaame@users.noreply.github.com>
2021-12-03 23:16:02 +0900

    4周年祭りの告知を追加

--- docs/sw.js
+++ docs/sw.js
@@ -1,46 +1,49 @@
-const VERSION = "1.6.0.3";
+const VERSION = "1.6.0.4";
 const CACHE_NAME = `salmonrun_time_timer-${VERSION}`;
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        `./`,
-        `./index.html`,
-        `./favicon.png`,
-        `./bundle.js`,
-        `./styles.css`,
-        `./sounds/1.wav`,
-        `./sounds/2.wav`,
-        `./sounds/3.wav`,
-        `./sounds/4.wav`,
-        `./sounds/5.wav`,
-        `./sounds/10.wav`,
-        `./sounds/30.wav`,
-        `./sounds/quite-impressed.mp3`,
-      ])
-        .then(() => self.skipWaiting());
-    })
-  );
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache
+                .addAll([
+                    `./`,
+                    `./index.html`,
+                    `./favicon.png`,
+                    `./bundle.js`,
+                    `./styles.css`,
+                    `./sounds/1.wav`,
+                    `./sounds/2.wav`,
+                    `./sounds/3.wav`,
+                    `./sounds/4.wav`,
+                    `./sounds/5.wav`,
+                    `./sounds/10.wav`,
+                    `./sounds/30.wav`,
+                    `./sounds/quite-impressed.mp3`,
+                ])
+                .then(() => self.skipWaiting());
+        })
+    );
 });
 
-self.addEventListener('activate', function (evt) {
-  evt.waitUntil(
-    caches.keys().then(function (keys) {
-      var promises = [];
-      keys.forEach(function (cacheName) {
-        if (cacheName != CACHE_NAME)
-          promises.push(caches.delete(cacheName));
-      });
-      return Promise.all(promises);
-    }));
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(function (keys) {
+            var promises = [];
+            keys.forEach(function (cacheName) {
+                if (cacheName != CACHE_NAME)
+                    promises.push(caches.delete(cacheName));
+            });
+            return Promise.all(promises);
+        })
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.match(event.request, { ignoreSearch: true }))
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.match(event.request, { ignoreSearch: true }))
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
 });
