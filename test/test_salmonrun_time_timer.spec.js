388f60454f492897e95e70f85e914547028d0a43
ema <ameema@gmail.com>
2022-08-10 23:37:25 +0900

    feat: 基点と間隔を変えられるようにした fix: update packages fix: テストを修正

--- test/test_salmonrun_time_timer.spec.js
+++ test/test_salmonrun_time_timer.spec.js
@@ -6,8 +6,8 @@ describe('SalmonrunTimeTimer', () => {
 
     const date_check_helper = (list, text_list) => {
         expect(list.length).to.eql(text_list.length);
-        for(let i in list.length) {
-            expect(list[i] - Date.parse(text_list[i])).to.eql(0);
+        for(let i = 0; i < list.length; ++i) {
+            expect(list[i] - Date.parse(text_list[i])).to.eql(0, list[i]);
         }
     };
 
@@ -63,6 +63,30 @@ describe('SalmonrunTimeTimer', () => {
                  '2018-01-01T00:26:00+09:00', 
                  '2018-01-01T00:34:00+09:00', 
                  '2018-01-01T00:42:00+09:00']);
+        });
+        it('9分間隔', () => {
+            const date = Date.parse('2017-12-31T23:46:00+09:00');
+            const list = timer.listup_next_STT(date, 2, 9);
+            date_check_helper(list,
+                ['2017-12-31T23:47:00+09:00',
+                 '2018-01-01T00:02:00+09:00',
+                 '2018-01-01T00:11:00+09:00', 
+                 '2018-01-01T00:20:00+09:00', 
+                 '2018-01-01T00:29:00+09:00', 
+                 '2018-01-01T00:38:00+09:00', 
+                 '2018-01-01T00:47:00+09:00']);
+        });
+        it('10分間隔', () => {
+            const date = Date.parse('2017-12-31T23:40:00+09:00');
+            const list = timer.listup_next_STT(date, 2, 10);
+            date_check_helper(list,
+                ['2017-12-31T23:42:00+09:00',
+                 '2018-01-01T00:02:00+09:00',
+                 '2018-01-01T00:12:00+09:00', 
+                 '2018-01-01T00:22:00+09:00', 
+                 '2018-01-01T00:32:00+09:00', 
+                 '2018-01-01T00:42:00+09:00', 
+                 '2018-01-01T01:02:00+09:00']);
         });
     });
 });
